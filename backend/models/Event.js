e828f4d892009b899d18610b03767715005ebc52
VairaSimone <158765247+VairaSimone@users.noreply.github.com>
2025-07-23 19:07:02 +0200

    snakebee - node 1.3

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const eventSchema = new mongoose.Schema({
   reptile: { type: mongoose.Schema.Types.ObjectId, ref: 'Reptile', required: true },
-  type: { type: String, enum: ['shed', 'feces', 'vet'], required: true },
+  type: { type: String, enum: ['shed', 'feces', 'vet', 'weight'], required: true },
   date: { type: Date, required: true },
   notes: { type: String },
+  weight: { type: Number }
 }, {
   timestamps: true
 });
